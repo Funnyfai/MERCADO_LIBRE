9fefbc6a47bcf5b1cacc5db2e52c21e6b6b107d0
Ana Sofia M.M <anasmoli05@gmail.com>
2025-11-11 07:19:22 -0500

    crud servicios de products

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import type { Product } from '../types/Products';
+import Button from './Button';
+
+interface ProductCardProps {
+  product: Product;
+  onEdit: (product: Product) => void;
+  onDelete: (productId: number) => void;   
+  }
+
+
+const ProductCard: React.FC<ProductCardProps> = ({ product, onEdit, onDelete }) => {
+  return ( 
+    <div className="border border-grey-200 rouded-lg p-4 shadow-sm hover:shadow-md transition-shadow">
+      {product.image && (
+         <img src={product.image} 
+           alt={product.name} 
+           className="w-full h-48 object-cover mb-4 rounded" 
+         />
+      )}
+      <h3 className="text-lg font-semibold text-gray-800 mb-2">{product.name}</h3>
+      <p className="text-gray-600 mb-2">${product.description}</p>
+      <p className="text-sm text-gray-500 mb-2">Categoría: {product.category}</p>
+        <p className="text-xl font-bold text-gray-600 mb-4">${product.price}</p>
+        <div className="flex space-x-2">
+            <Button variant="secondary" onClick={()=> onEdit(product)} className="flex-1">Editar</Button>
+            <Button variant="danger" onClick={()=> onDelete(product.id)} className="flex-1">Eliminar</Button>
+            </div>
+    </div>
+  );
+}
+ 
+export default ProductCard;
